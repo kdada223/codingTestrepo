fe5e443d71c0c4fd773f593bbde6625aeb0a5ef9
CoCookNam <58898852+kdada223@users.noreply.github.com>
2025-11-25 17:36:31 +0900

    feat: 입문문제 더 추가했음

--- programmers/2025-11-24/입문문제모음집1.js
+++ programmers/2025-11-24/입문문제모음집1.js
@@ -190,3 +190,34 @@ function solution(n, t) {
     return n*Math.pow(2,t);
 }
 //Math.pow를 활용한 방법
+
+
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요
+function solution(numbers) {
+    var answer = 0;
+    numbers.sort((a,b)=> b-a);
+    
+    const first = numbers[0];
+    const second = numbers[1];
+    
+    answer = first * second;
+   
+    return answer;
+}
+
+//코드상 나랑 비슷하지만 더 줄일 수 있어보여서 가져왔다
+function solution(numbers) {
+    numbers.sort((a,b)=>b-a);
+    return numbers[0]*numbers[1];
+}
+
+//머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+function solution(array, height) {
+    var answer = 0;
+    array.forEach(item => {
+       if(item > height){
+           answer += 1;
+       }
+    }) 
+    return answer;
+}
