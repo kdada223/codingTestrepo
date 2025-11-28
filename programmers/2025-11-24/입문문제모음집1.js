5e724a94e55e4a7f1e52cd7143cacf8ec85a8a34
CoCookNam <58898852+kdada223@users.noreply.github.com>
2025-11-28 16:22:38 +0900

    feat : Implement solution function for doubling array elements
    Add solution function to double elements in an array

--- programmers/2025-11-24/입문문제모음집1.js
+++ programmers/2025-11-24/입문문제모음집1.js
@@ -325,3 +325,8 @@ function solution(my_string, n) {
     console.log(answer);
     return answer;
 }
+
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+function solution(numbers) {
+    return numbers.map(a=> a * 2);
+}
