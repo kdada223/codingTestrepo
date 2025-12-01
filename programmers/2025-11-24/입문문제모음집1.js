71a6cdc8785ce1c2f2d4fd1f988259a26fa9a06d
CoCookNam <58898852+kdada223@users.noreply.github.com>
2025-12-01 16:34:46 +0900

    feat : Implement function to sort numbers from string
    Add solution function to extract and sort numbers from a string.

--- programmers/2025-11-24/입문문제모음집1.js
+++ programmers/2025-11-24/입문문제모음집1.js
@@ -358,3 +358,11 @@ function solution(array, n) {
     return answer;
 }
 
+//문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+function solution(my_string) {
+    var answer = [];
+    const numbers = my_string.replace(/[^0-9]/g, '');
+    answer = String(numbers).split('').map(Number);
+    answer.sort((a,b)=> a-b)
+    return answer;
+}
