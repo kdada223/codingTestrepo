11aeaeb954ec80f8bba218b0b83b991d24b0628b
CoCookNam <58898852+kdada223@users.noreply.github.com>
2025-12-01 10:03:40 +0900

    feat : Implement solution functions for string and array tasks
    Added multiple solution functions for various problems.

--- programmers/2025-11-24/입문문제모음집1.js
+++ programmers/2025-11-24/입문문제모음집1.js
@@ -336,3 +336,25 @@ function solution(my_string) {
     let result = my_string.replace(/[aeiou]/g, '')
     return result;
 }
+
+//정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+function solution(n) {
+    var answer = 0;
+    answer = n.toString().split('').map(Number);
+    let number = 0;
+    answer.forEach(item =>{
+        number += item;
+    })
+    return number;
+}
+//정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+function solution(array, n) {
+    var answer = 0;
+    array.forEach(item=>{
+       if(item == n){
+           answer++;
+       }
+    })
+    return answer;
+}
+
